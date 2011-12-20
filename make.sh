d5991c41bde87700c6bec45ca6ba20a8f9e7e1d0
Chris Czub <chris.czub@gmail.com>
2011-12-19 20:46:06 -0500

    use $BASEDIR in make

--- make.sh
+++ make.sh
@@ -16,46 +16,46 @@ while read line; do
     elif [ $jsflag == 1 ]; then
         if [ "$line" != "" ]; then
             echo "    Including JS $line"
-            cat js/$line.js >> _site/js.big.js
+            cat js/$line.js >> $BASEDIR/js.big.js
         fi
     elif [ "$line" == "[JS]" ]; then
         jsflag=1
-        echo "<script type='text/javascript' src='js.js'></script>" >> _site/index.html
+        echo "<script type='text/javascript' src='js.js'></script>" >> $BASEDIR/index.html
     elif [ "$line" == "[/LESS]" ]; then
         lessflag=0
     elif [ $lessflag == 1 ]; then
         if [ "$line" != "" ]; then
             echo "    Including LESS $line"
-            echo `lessc less/$line.less` >> _site/css.css
+            echo `lessc "less/$line.less"` >> $BASEDIR/css.css
         fi
     elif [ "$line" == "[LESS]" ]; then
         lessflag=1
-        for n in `ls css/*.css css/**/*.css`; do echo "<link href='$n' rel='stylesheet' type='text/css'>" >> _site/index.html; done
-        echo "<link href='css.css' rel='stylesheet' type='text/css'>" >> _site/index.html
+        for n in `ls css/*.css css/**/*.css`; do echo "<link href='$n' rel='stylesheet' type='text/css'>" >> $BASEDIR/index.html; done
+        echo "<link href='css.css' rel='stylesheet' type='text/css'>" >> $BASEDIR/index.html
     elif [ "$line" == "[/TEMPLATES]" ]; then
         templateflag=0
     elif [ $templateflag == 1 ]; then
         if [ "$line" != "" ]; then
             echo "    Including template $line"
-            cat templates/$line.html >> _site/index.html
+            cat templates/$line.html >> $BASEDIR/index.html
         fi
     elif [ "$line" == "[TEMPLATES]" ]; then
         templateflag=1
     else
-        echo "$line" >> _site/index.html
+        echo "$line" >> $BASEDIR/index.html
     fi
 done < "$INDEX"
 
 echo "Minifying JS..."
-uglifyjs _site/js.big.js > _site/js.js
-#rm _site/js.big.js
-mv _site/js.big.js _site/js.js
+uglifyjs $BASEDIR/js.big.js > $BASEDIR/js.js
+#rm $BASEDIR/js.big.js
+mv $BASEDIR/js.big.js $BASEDIR/js.js
 
 echo "Copying images..."
-cp -r img _site/img
+cp -r img $BASEDIR/img
 
 echo "Copying CSS..."
-cp -r css _site/css
+cp -r css $BASEDIR/css
 
 echo "Copying fonts..."
-cp -r fonts _site/fonts
+cp -r fonts $BASEDIR/fonts
